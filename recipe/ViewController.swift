ead8c5818c0c7bcf1da7e6e4059c887ee6de2a5f
205-5 <a205-5@205-5s-mac-mini.local>
2015-11-17 21:42:06 -0500

    added backgrounds

--- recipe/ViewController.swift
+++ recipe/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.rowHeight = 70
+        tableView.backgroundView = UIImageView(image: UIImage(named:"1"))
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationController?.navigationBar.alpha = 0.5
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,10 +37,30 @@ class ViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("customcell")! as UITableViewCell
+        
+        if (indexPath.item % 2 == 0){
+            cell.backgroundColor = UIColor.clearColor()
+        }
+        else {
+            cell.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.2)
+            cell.textLabel?.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.0)
+        }
+        cell.textLabel?.textColor = UIColor.whiteColor()
         cell.textLabel?.text = array[indexPath.item]
         return cell
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
