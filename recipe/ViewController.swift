f65607e0be624ffbe95462374d9a21215ce0ec94
205-5 <a205-5@205-5s-mac-mini.local>
2015-11-17 21:04:03 -0500

    initial project structure

--- recipe/ViewController.swift
+++ recipe/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let array = ["item1", "item2", "item3"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+        
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("customcell")! as UITableViewCell
+        cell.textLabel?.text = array[indexPath.item]
+        return cell
     }
 
 
