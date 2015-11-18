cee2392a9cccf2cf411417a1ba77d8ca7c961beb
205-5 <a205-5@205-5s-mac-mini.local>
2015-11-17 20:36:35 -0500

    Initial Commit

---
+++ recipe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recipe
+//
+//  Created by 205-5 on 2015-11-17.
+//  Copyright © 2015 MarcBueno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
